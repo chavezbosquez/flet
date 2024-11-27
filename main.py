022630b2d31fa50cdc4910b529e898c0a4d0ce20
Oscar Chávez-Bosquez <chavezbosquez@users.noreply.github.com>
2024-11-27 09:40:22 -0600

    from panel_inicio import PanelInicio

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
 import flet as ft
-
-class PanelInicio(ft.Pagelet):
-
-    def __init__(self):
-        super().__init__(self)
-        img_ujat = ft.Image(src="img/dacyti.png", width=200, fit=ft.ImageFit.CONTAIN)
-        txt_sistema = ft.Text("Tutorial de programación avanzada en Flet", size=30,
-                              font_family="RobotoSlab", weight=ft.FontWeight.W_500,
-                              color=ft.colors.GREEN_700)
-        txt_version = ft.Text("Versión 1.0", size=20,
-                              font_family="RobotoSlab", weight=ft.FontWeight.W_500,
-                              color=ft.colors.GREEN_700)
-        txt_info = ft.Text("Desarrollado con ❤️ por XYZ",
-                           theme_style=ft.TextThemeStyle.BODY_LARGE)
-
-        self.content=ft.Row([ft.Column([img_ujat, txt_sistema, txt_version, txt_info],
-                                       alignment=ft.MainAxisAlignment.CENTER,
-                                       horizontal_alignment=ft.CrossAxisAlignment.CENTER)],
-                            alignment=ft.MainAxisAlignment.CENTER,
-                            vertical_alignment=ft.CrossAxisAlignment.CENTER)
+from panel_inicio import PanelInicio
 
 def main(page: ft.Page):
 
